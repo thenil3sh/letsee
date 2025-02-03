fe9da94d2456b88cad9da187eca478492911a240
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-02-03 21:11:20 +0530

    yes

---
+++ pkg/letsee_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const slint_qt_get_widget: (a: number) => number;
+export const slint_mock_elapsed_time: (a: bigint) => void;
+export const slint_get_mocked_time: () => bigint;
+export const slint_send_mouse_click: (a: number, b: number, c: number) => void;
+export const slint_send_keyboard_char: (a: number, b: number, c: number) => void;
+export const send_keyboard_string_sequence: (a: number, b: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_5: WebAssembly.Table;
+export const closure316_externref_shim: (a: number, b: number, c: any) => void;
+export const closure790_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h686621cf39b6cc7d: (a: number, b: number) => void;
+export const closure791_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure858_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
